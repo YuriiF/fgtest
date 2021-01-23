3f2808793d5ab2c3b7894f0c69e395c0ec19e9d0
Yurii Fotul <fotul.yurii@gmail.com>
2021-01-23 15:58:07 +0100

    * generate interfaces library for stock app * create user-interface

---
+++ libs/stock/interfaces/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'stock-interfaces',
+  preset: '../../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../coverage/libs/stock/interfaces',
+};
