3c68696e6b02fb31a8f15ec70ab32296ac9f6b5b
Yurii Fotul <fotul.yurii@gmail.com>
2021-01-23 16:06:31 +0100

    use fake server only in development

--- apps/stock/src/main.tsx
+++ apps/stock/src/main.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import { BrowserRouter } from 'react-router-dom';
+import { createServer } from '@fgtest/util';
 
+/** Custom imoprts */
 import App from './app/App';
+
+const { NODE_ENV: isDev } = process.env;
+
+if (isDev === 'development') {
+  createServer();
+}
 
 ReactDOM.render(
   <React.StrictMode>
