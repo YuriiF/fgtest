7cb945b2266d3e4181441808cdaad14033e6ebb2
Yurii Fotul <fotul.yurii@gmail.com>
2021-01-23 11:11:57 +0100

    Remove generated code, add few more packages date handling, fetch handling

--- apps/stock/src/app/App.spec.tsx
+++ apps/stock/src/app/App.spec.tsx
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react';
 
 import { BrowserRouter } from 'react-router-dom';
 
-import App from './app';
+import App from './App';
 
 describe('App', () => {
   it('should render successfully', () => {
@@ -23,6 +23,6 @@ describe('App', () => {
       </BrowserRouter>
     );
 
-    expect(getByText('Welcome to stock!')).toBeTruthy();
+    expect(getByText('App')).toBeTruthy();
   });
 });
