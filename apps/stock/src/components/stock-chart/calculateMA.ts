9245c61579c619984503de2ef98fbecf7be5f7e5
Yurii Fotul <fotul.yurii@gmail.com>
2021-01-27 12:08:44 +0100

    moving avarege calculation for prediction purposes

---
+++ apps/stock/src/components/stock-chart/calculateMA.ts
@@ -0,0 +1,18 @@
+export function calculateMA(dayCount: number, data: string | any[]) {
+  let result = [];
+  for (let i = 0, len = data.length; i < len; i++) {
+    if (i < dayCount) {
+      result.push('-');
+      continue;
+    }
+
+    let sum = 0;
+
+    for (let j = 0; j < dayCount; j++) {
+      sum += data[i - j][1];
+    }
+
+    result.push((sum / dayCount).toFixed(2));
+  }
+  return result;
+}
