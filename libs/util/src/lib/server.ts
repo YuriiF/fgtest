467e8c0b98ffd1dc33416394e3357f49a478167e
Yurii Fotul <fotul.yurii@gmail.com>
2021-01-27 11:44:10 +0100

    feat: create miragejs server for fake users login and favorites by user, default user: admin, pass: admin, no intercept finnhub API

--- libs/util/src/lib/server.ts
+++ libs/util/src/lib/server.ts
@@ -1,40 +1,49 @@
-import { Server, Model, Factory, Response } from 'miragejs';
-import { signin, signup } from './routes/user';
-
-export const handleError = (error: unknown, message = 'Error ocurred') => {
-  return new Response(400, undefined, {
-    data: {
-      message,
-      error: true,
-    },
-  });
-};
+import { Server, Model, Factory, belongsTo, hasMany } from 'miragejs';
+import { login, signup } from './routes/user';
+import * as favorite from './routes/favorite';
 
 export const createServer = (env?: string): Server => {
   return new Server({
     environment: env ?? 'development',
 
     models: {
-      user: Model,
+      favorite: Model.extend({
+        user: belongsTo(),
+      }),
+      user: Model.extend({
+        favorite: hasMany(),
+      }),
     },
 
     factories: {
       user: Factory.extend({
         username: 'admin',
-        password: 'password',
+        password: 'admin',
         email: 'admin@example.com',
+      }),
+      favorite: Factory.extend({
+        stockCode: 'GOOG',
+        userId: '1',
       }),
     },
 
     seeds: (server) => {
       server.create('user');
+      server.create('favorite');
     },
 
     routes(): void {
       this.urlPrefix = 'https://stock.app';
 
-      this.post('/user/signin', signin);
-      this.post('/user/signup', signup);
+      /** Pass through finnhub api URI Origin */
+      this.passthrough('https://finnhub.io/api/**');
+
+      this.post('/auth/login', login);
+      this.post('/auth/signup', signup);
+
+      this.get('/favorites/:id', favorite.getFavorites);
+      this.post('/favorites/', favorite.create);
+      this.put('/favorites/:id', favorite.updateFavorite);
     },
   });
 };
