5e8ee2a2067cd792579a4fff93303236f910b025
Yurii Fotul <fotul.yurii@gmail.com>
2021-01-27 11:45:07 +0100

    miragejs routes to handling sighnup, login

---
+++ libs/util/src/lib/routes/favorite.ts
@@ -0,0 +1,68 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Response, Request } from 'miragejs';
+import { handleError } from '../errors/handle-error';
+import { Favorite, User } from '@fgtest/stock/interfaces';
+
+import dayjs from 'dayjs';
+
+export const create = (
+  schema: any,
+  req: Request,
+): { user: User; favorite: Favorite } | Response => {
+  try {
+    const { stockCode, userId } = JSON.parse(
+      req.requestBody,
+    ) as Partial<Favorite>;
+    const exUser = schema.users.findBy({ id: userId });
+
+    if (!exUser) {
+      return handleError(null, 'No such user exists.');
+    }
+
+    const now = dayjs().format();
+    const favorite = exUser.createFavorite({
+      stockCode,
+      createdAt: now,
+      updatedAt: now,
+    });
+
+    return {
+      user: {
+        ...exUser.attrs,
+      },
+      favorite: favorite.attrs,
+    };
+  } catch (error) {
+    return handleError(error, 'Failed to create Favorite.');
+  }
+};
+
+export const updateFavorite = (
+  schema: any,
+  req: Request,
+): Favorite | Response => {
+  try {
+    const favorite = schema.favorites.find(req.params.id);
+    const data = JSON.parse(req.requestBody) as Partial<Favorite>;
+    const now = dayjs().format();
+    favorite.update({
+      ...data,
+      updatedAt: now,
+    });
+    return favorite.attrs as Favorite;
+  } catch (error) {
+    return handleError(error, 'Failed to update Favorite.');
+  }
+};
+
+export const getFavorites = (
+  schema: any,
+  req: Request,
+): Favorite[] | Response => {
+  try {
+    const user = schema.users.find(req.params.id);
+    return user.favorite as Favorite[];
+  } catch (error) {
+    return handleError(error, 'Could not get user favorites.');
+  }
+};
--- libs/util/src/lib/routes/user.ts
+++ libs/util/src/lib/routes/user.ts
@@ -1,36 +1,37 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Response, Request } from 'miragejs';
-import { handleError } from '../server';
+import { handleError } from '../errors/handle-error';
 import { User } from '@fgtest/stock/interfaces';
 import { nanoid } from 'nanoid';
 
 const generateToken = () => nanoid();
 
-export interface UserResponse {
+export interface AuthResponse {
   token: string;
   user: User;
 }
 
-export const signin = (schema: any, req: Request): UserResponse | Response => {
+export const login = (schema: any, req: Request): AuthResponse | Response => {
   const { username, password } = JSON.parse(req.requestBody);
   const user = schema.users.findBy({ username });
 
   if (!user) {
-    return handleError(null, 'Username is incorrect');
+    return handleError(null, 'No user with that username exists');
   }
 
   if (password !== user.password) {
-    return handleError(null, 'Wrong password');
+    return handleError(null, 'Password is incorrect');
   }
 
   const token = generateToken();
+
   return {
     user: user.attrs as User,
     token,
   };
 };
 
-export const signup = (schema: any, req: Request): UserResponse | Response => {
+export const signup = (schema: any, req: Request): AuthResponse | Response => {
   const data = JSON.parse(req.requestBody);
   const exUser = schema.users.findBy({ username: data.username });
   if (exUser) {
@@ -45,6 +46,6 @@ export const signup = (schema: any, req: Request): UserResponse | Response => {
 };
 
 export default {
-  signin,
+  login,
   signup,
 };
